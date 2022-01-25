6091a9098a5e2294920702fff4e5330f7af5b916
Adam Yang <ayang@wpi.edu>
2022-01-25 18:50:27 -0500

    reorganized files, made separate classes for different components of project

---
+++ assignment_1/src/com/company/AStar.java
@@ -0,0 +1,39 @@
+package com.company;
+
+
+public class AStar {
+    private int x_coord;
+    private int y_coord;
+    public AStar() {
+        this.x_coord = 0;
+        this.y_coord = 0;
+    }
+
+
+    public int search() {
+
+//        frontier = PriorityQueue()
+//        frontier.put(start, 0)
+//        came_from = dict()
+//        cost_so_far = dict()
+//        came_from[start] = None
+//        cost_so_far[start] = 0
+//
+//        while not frontier.empty():
+//            current = frontier.get()
+//
+//            if current == goal:
+//                break
+//
+//            for next in graph.neighbors(current):
+//                new_cost = cost_so_far[current] + graph.cost(current, next)
+//                if next not in cost_so_far or new_cost < cost_so_far[next]:
+//                    cost_so_far[next] = new_cost
+//                    priority = new_cost + heuristic(goal, next)
+//                    frontier.put(next, priority)
+//                    came_from[next] = current
+//
+        return 1;
+    }
+
+}
--- assignment_1/src/com/company/Board.java
+++ assignment_1/src/com/company/Board.java
@@ -5,12 +5,12 @@ package com.company;
  * Lena Dias, Roopsa Ghosh, Adam Yang
  * 1/25/2022
  */
+
 import java.io.*;
 import java.util.Scanner;
 import java.lang.*;
 
-
-public class Assignment_1 {
+public class Board {
     public static void main(String[] args) throws IOException {
         final int ROW = 3;
         final int COL = 4;
@@ -26,15 +26,15 @@ public class Assignment_1 {
             //Scan file TODO refactor to auto-configure row/col?
             for (int i = 0; i < ROW; i++)
             {
-                    for (int j = 0; j < COL; j++)
+                for (int j = 0; j < COL; j++)
+                {
+                    if(sc.hasNext())
                     {
-                        if(sc.hasNext())
-                        {
-                            char ch = sc.next().charAt(0); // Convert to char
-                            level[i][j] = ch;
-                            System.out.println(level[i][j]);
-                        }
+                        char ch = sc.next().charAt(0); // Convert to char
+                        level[i][j] = ch;
+                        System.out.println(level[i][j]);
                     }
+                }
             }
             sc.close();
 
@@ -45,5 +45,6 @@ public class Assignment_1 {
             err.printStackTrace();
         }
     }
+
 
 }
