0c998f92998e86c960b38c76496b2eee541c149f
Adam Yang <ayang@wpi.edu>
2022-01-27 11:18:28 -0500

    worked on a_star_search

--- assignment_1/src/com/company/AStar.java
+++ assignment_1/src/com/company/AStar.java
@@ -18,50 +18,41 @@ public class AStar {
         }
     });
 
-    public int search() {
+    public Coordinate search(Coordinate start_point) {
         // Priority queue for Coordinates to visit
         PriorityQueue<Coordinate> OPEN = new PriorityQueue<Coordinate>(CoordinateComparator);
 
-        OPEN.add(new Coordinate(200, 2, 'c', 5));
-        OPEN.add(new Coordinate(40, 4, 'b', 2));
-        OPEN.add(new Coordinate(3, 3, 'a', 10));
+        // Adds start_point to priority queue
+        OPEN.add(start_point);
 
-//        // Remove items from the Priority Queue (DEQUEUE)
-        while (!OPEN.isEmpty()) {
-            Coordinate e = OPEN.remove();
-            System.out.print(e.getX());
-            System.out.print(' ');
-            System.out.print(e.getY());
-            System.out.print(' ');
-            System.out.print(e.getComplexity());
-            System.out.print(' ');
-            System.out.println(e.getPriority());
+        // initialize arrays to hold previous nodes and current path cost to coordinates
+        Coordinate[][] came_from = new Coordinate[][];
+        int[][] cost_so_far = new int[][];
+
+        // set previous node of start to null, and current path cost of start to 0
+        came_from[start_point.getX()][start_point.getY()] = null;
+        cost_so_far[start_point.getX()][start_point.getY()] = 0;
+
+        while(OPEN.size() != 0) {
+            // get coordinate with most priority
+            Coordinate current = OPEN.remove();
+
+            // if goal is reached, return path
+            if(current.getComplexity() == 'G') {
+                return current;
+            }
+//
+//          Neighbors = board.getNeighbors(current);
+            for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+            cost_so_far[next] = new_cost
+            priority = new_cost + heuristic(goal, next)
+            frontier.put(next, priority)
+            came_from[next] = current
         }
 
 
-
-//        OPEN.insert(gameboard.start, 0);
-//        came_from = dict()
-//        cost_so_far = dict()
-//        came_from[start] = None
-//        cost_so_far[start] = 0
-//
-//        while(OPEN.size() != 0):
-//            Coordinate current = OPEN.remove();
-////
-//            if(current.getComplexity() == 'G') {
-//                return 1;
-//            }
-////
-////          Neighbors = board.getNeighbors(current);
-//            for next in graph.neighbors(current):
-//                new_cost = cost_so_far[current] + graph.cost(current, next)
-//                if next not in cost_so_far or new_cost < cost_so_far[next]:
-//                    cost_so_far[next] = new_cost
-//                    priority = new_cost + heuristic(goal, next)
-//                    frontier.put(next, priority)
-//                    came_from[next] = current
-//
         return 1;
     }
 
